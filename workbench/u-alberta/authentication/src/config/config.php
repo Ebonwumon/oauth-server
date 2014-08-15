6a8dea661ab992c4f15dcb1084140db34fcbd73d
Troy Pavlek <ebonwumon@depotwarehouse.net>
2014-08-15 10:49:26 -0600

    Changed from workbench to package for authentication

--- workbench/u-alberta/authentication/src/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'ldap_host' => 'ldaps://directory.srv.ualberta.ca',
-    'ldap_port' => 636,
-
-    'ldap_service_user' => 'busdisa',
-    'ldap_service_password' => "2CCc@c$(5&de",
-);
